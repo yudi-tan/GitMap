d0b3564d81c6c2758b4711f88654f6a7ffcc9bf8
Yudi Tan <yudi98@berkeley.edu>
2017-12-24 00:05:05 -0600

    POST request completed

--- gitmap-server/app.js
+++ gitmap-server/app.js
@@ -1,6 +1,12 @@
+var bodyParser = require('body-parser');
+
 var express = require('express');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require('mongoose');
 
@@ -20,12 +26,28 @@ app.get('/projects', function(req, res) {
   projectsModel.find({}, function(err, proj) {
     if(err){
       console.log(err);
-      res.send('error');
+      res.send('error in GET');
     } else {
         res.send(proj);
     }
     })
 });
+
+app.post('/newproject', function(req, res) {
+  var data = {
+    username: req.body.username,
+    url: req.body.url,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude
+  };
+  projectsModel.create(data, (err, proj) => {
+    if (err) {
+      res.send('error in POST');
+    } else {
+      res.send(data);
+    };
+  });
+})
 
 var server = app.listen(5000, function() {
   console.log('Express server listening on port ' + 5000);
