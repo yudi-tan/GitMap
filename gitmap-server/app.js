ad78f627d85c9636dc3189a28abd35d939625a37
Yudi Tan <yudi98@berkeley.edu>
2017-12-24 00:49:19 -0600

    updated express CORS

--- gitmap-server/app.js
+++ gitmap-server/app.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var app = express();
 
+var cors = require('cors');
+
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
