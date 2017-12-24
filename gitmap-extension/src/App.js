2ee9ba9ce02b8e8cc36a17e4474436d7ff57e807
Yudi Tan <yudi98@berkeley.edu>
2017-12-24 16:32:39 -0600

    added geolocation

--- gitmap-extension/src/App.js
+++ gitmap-extension/src/App.js
@@ -1,38 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import gh from 'parse-github-url';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data: {},
+      done: false
     }
   }
 
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      this.setState({done: false, latitude: position.coords.latitude, longitude: position.coords.longitude});
+    }.bind(this));
+  }
+
+
   saveProject() {
-    axios.post('http://localhost:5000/newproject', {
-      username: 'riceball1',
-      url: 'www.github111.com',
-      latitude: '1231242412',
-      longitude: '1312312'
+
+    axios.post('http://gitmap.us-west-1.elasticbeanstalk.com/newproject', {
+      username: 'mrtadsads',
+      url: 'www.ooh.com',
+      latitude: this.state.latitude,
+      longitude: this.state.longitude
     }).then(function(response) {
-      console.log(response.data);
-    }).catch(function(error) {
+      this.setState({done: true});
+    }.bind(this)).catch(function(error) {
       console.log(error);
     });
   }
 
   render() {
-    console.log(this.state.data);
-    return (
-      <div className="App">
-        <h3>Welcome to GitMap!</h3>
-        <p>Click on the button below to share your open source initiative with developers near you!</p>
-        <button onClick={() => this.saveProject()}>Save</button>
-      </div>
-    );
+
+    if (!this.state.done) {
+      return (
+        <div className="App">
+          <h3>Welcome to GitMap!</h3>
+          <p>Click on the button below to share your open source initiative with developers near you!</p>
+          {(this.state.latitude && this.state.longitude) ? <button onClick={() => this.saveProject()}>Save</button>
+          : <p>Loading...</p>}
+
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <p> Done! Download the app to view projects near you!</p>
+        </div>
+
+      )
+    }
+
   }
 }
 
